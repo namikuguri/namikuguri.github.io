e5036676d2490d188adc57625816c75a3b7e6c48
namikuguri <zozo4869@gmail.com>
2018-02-22 16:38:36 +0900

    Haml導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var haml = require('gulp-ruby-haml');
 var sass = require('gulp-sass');
 
 // localhost:8080を立ち上げる
@@ -10,19 +11,27 @@ gulp.task('connect', function() {
   });
 });
 
-// SassとCSSのコンパイル関係を指定
+// Hamlのコンパイル
+gulp.task('haml', function() {
+  gulp.src('./src/**/*.haml').
+       pipe(haml()).
+       pipe(gulp.dest('./'));
+});
+
+// Sassのコンパイル
 gulp.task('sass', function(){
   gulp.src('./src/style/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./style/'));
 });
 
-// Sass の自動監視
-gulp.task('sass-watch', ['sass'], function(){
-  var watcher = gulp.watch('./src/style/**/*.scss', ['sass']);
-  watcher.on('change', function(event) {
-  });
+// 自動監視
+gulp.task('watch', function(){
+  //Hamlの監視
+  gulp.watch('./src/**/*.haml', ['haml']);
+  //sassの監視
+  gulp.watch('./src/style/**/*.scss', ['sass']);
 });
 
 // `gulp` でlocalhost:8080の立ち上げとSassの監視を開始
-gulp.task('default', ['connect', 'sass-watch']);
+gulp.task('default', ['connect', 'watch']);
