cb32af024402c677b72dd02c5be502fdc06c186b
namikuguri <zozo4869@gmail.com>
2018-03-05 13:44:01 +0900

    HTMLとJSはそのまんま書く

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
-var fileinclude = require('gulp-file-include');
-var haml = require('gulp-ruby-haml');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
-var uglify = require('gulp-uglify');
 
 // localhost:8080を立ち上げる
 gulp.task('connect', function() {
@@ -12,23 +9,6 @@ gulp.task('connect', function() {
     root: './',
     livereload: true,
   });
-});
-
-// Hamlのコンパイル
-gulp.task('haml', function() {
-  gulp.src('./src/**/*.haml').
-    pipe(haml()).
-    pipe(gulp.dest('./'));
-});
-
-// ファイルの結合
-gulp.task('fileinclude', function() {
-  gulp.src(['./**/*.html', '!./**/*_.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./'));
 });
 
 // Sassのコンパイル
@@ -40,26 +20,17 @@ gulp.task('sass', function(){
 
 // 自動監視
 gulp.task('watch', function(){
-  // Hamlの監視
-  gulp.watch('./src/**/*.haml', ['haml']);
   // Sassの監視
   gulp.watch('./src/style/**/*.scss', ['sass']);
 });
 
 // 画像の軽量化
 gulp.task('minify-image', function(){
-  gulp.src('./src/image/*')
+  gulp.src('./image/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./image'))
 });
 
-// JavaScript の圧縮（軽量化）
-gulp.task('minify-js', function() {
-  return gulp.src("./src/script/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest('./script/'));
-});
-
 // `gulp` でlocalhost:8080の立ち上げとSassの監視を開始
 gulp.task('default', ['connect', 'watch']);
-gulp.task('build', ['minify-image', 'minify-js']);
+gulp.task('build', ['minify-image']);
--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
-!function(){"use strict";var l=$("#_draw-handler"),n=$("#_drawer");l.on("click",function(){l.toggleClass("globalMenu__button--open"),n.toggleClass("globalMenu__list--open")})}();
+(function() {
+  'use strict';
+
+  var $handler = $('#_draw-handler'),
+      $drawer = $('#_drawer');
+
+  $handler.on('click', function() {
+    $handler.toggleClass('globalMenu__button--open');
+    $drawer.toggleClass('globalMenu__list--open');
+  });
+})();
